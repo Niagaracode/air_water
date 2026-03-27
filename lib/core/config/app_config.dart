2c9bc58bb96c5c9d6f4d89643ff1bb7ac10dbf69
Karthikniagara <karthikniagara@gmail.com>
2026-03-27 17:39:52 +0530

    url-issue

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,11 +1,6 @@
 class AppConfig {
-<<<<<<< HEAD
-
-  static const String apiUrl = 'https://unsophisticated-hypernutritive-shela.ngrok-free.dev/api';
-  static const String mqttUrl = 'broker.yourdomain.com';
-=======
   static const String apiUrl = 'http://localhost:4000/api';
->>>>>>> karthik_dev
+  static const String mqttUrl = 'broker.yourdomain.com';
   static const String googleMapsApiKey =
       'AIzaSyBIsPNskLIYGQNBDajxsiMNdcB1NhgzFWI';
 }
