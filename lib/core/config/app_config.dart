a35ed671ce539a280eac6997341ecb2095902aac
Kamarajniagara <niagara.kamaraj@gmail.com>
2026-04-09 09:32:23 +0530

    code changes

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,7 +1,5 @@
 class AppConfig {
-
-  static const String apiUrl = 'https://unsophisticated-hypernutritive-shela.ngrok-free.dev/api';
+  static const String apiUrl = 'https://organ-significantly-treasury-shore.trycloudflare.com/api';
   static const String mqttUrl = 'broker.yourdomain.com';
-  static const String googleMapsApiKey =
-      'AIzaSyBIsPNskLIYGQNBDajxsiMNdcB1NhgzFWI';
+  static const String googleMapsApiKey = 'AIzaSyBIsPNskLIYGQNBDajxsiMNdcB1NhgzFWI';
 }
