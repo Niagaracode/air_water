5b5e3116ad4176acbba27d558f2156ec76a7a916
Kamarajniagara <niagara.kamaraj@gmail.com>
2026-05-06 10:12:37 +0530

    code updated

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  static const String apiUrl = 'https://sight-polyphonic-creature-depends.trycloudflare.com/api';
+  static const String apiUrl = 'https://pharmaceutical-touched-consequence-average.trycloudflare.com/api';
   static const String googleMapsApiKey = 'AIzaSyBIsPNskLIYGQNBDajxsiMNdcB1NhgzFWI';
 
   static const String mqttWebHost = 'wss://irrigationcare.niagaraautomation.com:9443/mqtt';
