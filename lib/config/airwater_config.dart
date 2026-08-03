29d63cc71275c2deda6b54b4aceedb7d7b16c846
Karthikniagara <karthikniagara@gmail.com>
2026-08-03 17:59:34 +0530

    update-code

--- lib/config/airwater_config.dart
+++ lib/config/airwater_config.dart
@@ -4,9 +4,9 @@ import 'app_config.dart';
 const airWaterConfig = AppConfig(
   appName: 'AIR WATER',
 
-  apiUrl: 'https://irrigationcare.niagaraautomation.com:4000/api',
+  apiUrl: 'http://localhost:4000/api',
 
-  // Web MQTT
+  // Web MQTTcd
   mqttWebHost: 'wss://irrigationcare.niagaraautomation.com:9443/mqtt',
   mqttWebPort: 9443,
 
